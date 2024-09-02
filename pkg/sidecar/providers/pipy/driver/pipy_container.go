57c77b2dad01c5659c0a40935dffe6fab520aa71
Cybwan <baili@flomesh.io>
2024-09-03 07:39:42 +0800

    optimized fsm connector.

--- pkg/sidecar/providers/pipy/driver/pipy_container.go
+++ pkg/sidecar/providers/pipy/driver/pipy_container.go
@@ -176,28 +176,18 @@ func getPipySidecarContainerSpec(injCtx *driver.InjectorContext, pod *corev1.Pod
 		trustDomain := injCtx.CertManager.GetTrustDomain()
 		ndots := "1"
 		searches := make([]string, 0)
-		if injCtx.Configurator.IsSearchesWithTrustDomain() {
-			ndots = "4"
-			if injCtx.Configurator.IsSearchesWithNamespace() {
-				if len(pod.Namespace) > 0 {
-					ndots = "5"
-					searches = append(searches, fmt.Sprintf("%s.svc.%s", pod.Namespace, trustDomain))
-				} else if len(injCtx.PodNamespace) > 0 {
-					ndots = "5"
-					searches = append(searches, fmt.Sprintf("%s.svc.%s", injCtx.PodNamespace, trustDomain))
-				}
-			}
-			searches = append(searches, fmt.Sprintf("svc.%s", trustDomain))
-			searches = append(searches, trustDomain)
-		} else if injCtx.Configurator.IsSearchesWithNamespace() {
-			if len(pod.Namespace) > 0 {
-				ndots = "2"
-				searches = append(searches, pod.Namespace)
-			} else if len(injCtx.PodNamespace) > 0 {
-				ndots = "2"
-				searches = append(searches, injCtx.PodNamespace)
-			}
+
+		ndots = "4"
+		if len(pod.Namespace) > 0 {
+			ndots = "5"
+			searches = append(searches, fmt.Sprintf("%s.svc.%s", pod.Namespace, trustDomain))
+		} else if len(injCtx.PodNamespace) > 0 {
+			ndots = "5"
+			searches = append(searches, fmt.Sprintf("%s.svc.%s", injCtx.PodNamespace, trustDomain))
 		}
+
+		searches = append(searches, fmt.Sprintf("svc.%s", trustDomain))
+		searches = append(searches, trustDomain)
 
 		pod.Spec.DNSConfig = &corev1.PodDNSConfig{
 			Nameservers: []string{"127.0.0.153"},
