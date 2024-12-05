fdfd6b857fc39fdea5ec3d4af605e67a816edbfd
Cybwan <baili@flomesh.io>
2024-12-05 10:31:57 +0800

    init zookeeper connector.
    Signed-off-by: Cybwan <baili@flomesh.io>

--- cmd/test/test.go
+++ cmd/test/test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/flomesh-io/fsm/pkg/zookeeper"
 	"github.com/flomesh-io/fsm/pkg/zookeeper/discovery"
+	"github.com/flomesh-io/fsm/pkg/zookeeper/discovery/nebula"
 )
 
 func main() {
@@ -13,13 +14,13 @@ func main() {
 		"zookeeperMetadataReport",
 		[]string{"127.0.0.1:2181"},
 		true,
-		zookeeper.WithZkTimeOut(time.Duration(time.Second*15)),
+		zookeeper.WithZkTimeOut(time.Second*15),
 	)
 	if err != nil {
 		panic(err)
 	}
 	basePath := "/Application/grpc"
-	sd := discovery.NewServiceDiscovery(client, basePath, nil)
+	sd := discovery.NewServiceDiscovery(client, basePath, new(nebula.Ops))
 	fmt.Println(sd.QueryForNames())
 	serviceInstances, err := sd.QueryForInstances("com.orientsec.demo.Greeter/providers")
 	fmt.Println(serviceInstances)
---
+++ pkg/zookeeper/discovery/dubbo/instance.go
@@ -0,0 +1,22 @@
+package dubbo
+
+type ServiceInstance struct {
+	serviceName string
+	instanceId  string
+}
+
+func (ins *ServiceInstance) ServiceName() string {
+	return ins.serviceName
+}
+
+func (ins *ServiceInstance) InstanceId() string {
+	return ins.instanceId
+}
+
+func (ins *ServiceInstance) Marshal() ([]byte, error) {
+	return nil, nil
+}
+
+func (ins *ServiceInstance) Unmarshal(data []byte) error {
+	return nil
+}
