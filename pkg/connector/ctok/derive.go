4201b8b8a3677ac0762cede024f57334e16bdb12
Cybwan <baili@flomesh.io>
2025-03-12 10:39:09 +0800

    connector supports headless endpoints
    Signed-off-by: Cybwan <baili@flomesh.io>

--- pkg/connector/ctok/derive.go
+++ pkg/connector/ctok/derive.go
@@ -1,22 +1,25 @@
 package ctok
 
 import (
-	apiv1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 
 	"github.com/flomesh-io/fsm/pkg/connector"
 	"github.com/flomesh-io/fsm/pkg/constants"
 )
 
 // IsSyncCloudNamespace if sync namespace
-func IsSyncCloudNamespace(ns *apiv1.Namespace) bool {
+func IsSyncCloudNamespace(ns *corev1.Namespace) bool {
 	if ns != nil {
-		_, exists := ns.Annotations[connector.AnnotationMeshServiceSync]
-		return exists
+		if len(ns.Annotations) > 0 && len(ns.Labels) > 0 {
+			_, meshMonitored := ns.Labels[constants.FSMKubeResourceMonitorAnnotation]
+			_, meshServiceSync := ns.Annotations[connector.AnnotationMeshServiceSync]
+			return meshMonitored && meshServiceSync
+		}
 	}
 	return false
 }
 
-func (s *CtoKSyncer) hasOwnership(service *apiv1.Service) bool {
+func (s *CtoKSyncer) hasOwnership(service *corev1.Service) bool {
 	return len(service.Labels) > 0 && len(service.Annotations) > 0 &&
 		service.Labels[constants.CloudSourcedServiceLabel] == True &&
 		service.Annotations[connector.AnnotationMeshServiceSyncManagedBy] == s.controller.GetConnectorUID()
